b2a90b7e259d37a88dd724ebd2ec8387adc2895f
ekinomicss <zorerekin@gmail.com>
2024-09-15 20:21:24 -0400

    add jest and config files

---
+++ config/elasticsearchConfig.ts
@@ -0,0 +1,7 @@
+import { Client } from '@elastic/elasticsearch';
+
+const elasticClient = new Client({
+    node: process.env.ELASTICSEARCH_URL || 'http://localhost:9200'
+});
+
+export default elasticClient;
---
+++ test/logService.test.ts
@@ -0,0 +1,7 @@
+import { storeLog } from '../src/app/services/logService';
+
+test('should store log in Elasticsearch', async () => {
+    const mockLog = { message: 'Test log' };
+    const result = await storeLog(mockLog);
+    expect(result).toBeDefined();
+});
